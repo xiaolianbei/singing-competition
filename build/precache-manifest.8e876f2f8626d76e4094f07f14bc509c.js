881f5ab821cea05f046b22665a351a79a9a44541
Chen <jonkiky@gmail.com>
2022-01-10 23:45:51 -0500

    build

---
+++ build/precache-manifest.8e876f2f8626d76e4094f07f14bc509c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd09c0f2fb13b5bfe87124580ece47b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "309e071adbe75f21db81",
+    "url": "./static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "309e071adbe75f21db81",
+    "url": "./static/js/2.ca08a9b4.chunk.js"
+  },
+  {
+    "revision": "afae2dee8348c2c548871bf98dff247b",
+    "url": "./static/js/2.ca08a9b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958eef0233e4016f53a6",
+    "url": "./static/js/main.4ccfa855.chunk.js"
+  },
+  {
+    "revision": "204507d50144289a9267",
+    "url": "./static/js/runtime-main.7a5c02b9.js"
+  }
+]);
